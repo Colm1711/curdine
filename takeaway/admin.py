476e8f5d3a6a4a3474d83ceca91717d463b64caa
Colm1711 <cosullivan1711@gmail.com>
2022-09-10 12:52:00 +0000

    Add auto slug generation and to Amdin a display, search fields and filters for Food item mgmt

--- takeaway/admin.py
+++ takeaway/admin.py
@@ -5,5 +5,19 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Food_item)
 class Food_itemAdmin(SummernoteModelAdmin):
+    '''
+    This is the class that controls the Admins view of the
+    food items.
 
+    List how the content is presented to Amdmin User and provides list filter 
+    panel.
+
+    Provides search fields.
+
+    Slug field is set to autopopulate on food name.
+    '''
+    list_display = ('food_name', 'description', 'price', 'slug')
+    search_fields = ['food_name', 'description', 'price']
+    prepopulated_fields = {'slug': ('food_name',)}
+    list_filter = ('food_name', 'price')
     summernote_fields = ('description')
