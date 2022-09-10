92e29efef009527add3d553a8ce9eb73d1407ff8
Colm1711 <cosullivan1711@gmail.com>
2022-09-10 13:02:15 +0000

    Add food item list to the views to order on menu page

--- takeaway/views.py
+++ takeaway/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Food_item
 
-# Create your views here.
+
+class Food_item_List(generic.ListView):
+    model = Food_item
+    queryset = Food_item.objects.filter().order_by('-food_name')
+    template_name = 'menu.html'
+    paginate_by = 6
