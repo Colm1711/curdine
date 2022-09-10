984290b00d00b445021aaf66ebb44e86ada77f20
Colm1711 <cosullivan1711@gmail.com>
2022-09-10 12:31:51 +0000

    Add Food_item to admin panel, add import fo summernote to description

--- takeaway/admin.py
+++ takeaway/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Food_item
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Food_item)
+class Food_itemAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description')
