025a2641ded8dc6d513a3046bf3327c33a59da97
Colm1711 <cosullivan1711@gmail.com>
2022-09-10 12:18:51 +0000

    Add food item to models, UX base for food item for View menu ticket

--- takeaway/models.py
+++ takeaway/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Food_item(models.Model):
+    '''
+    This is the food item model for items that user will
+    be able order from the website
+    '''
+    food_name = models.CharField(max_length=200, unique=True)
+    food_image = CloudinaryField('image', default='placeholder')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['-food_name']
+    
+    def __str__(self):
+        return self.food_name
