cf7fe233a8caf390be8a80e76603af8c89154459
Colm1711 <cosullivan1711@gmail.com>
2022-09-10 13:42:28 +0000

    Add Home and Food_item_list to views to render on urls

--- takeaway/views.py
+++ takeaway/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.views import generic
 from .models import Food_item
 
+class Home(TemplateView):
+    '''This allows the home page to be rendered to user'''
+    template_name = 'index.html'
 
 class Food_item_List(generic.ListView):
+    '''This allows the Menu page and ittems to be rendered to user'''
     model = Food_item
     queryset = Food_item.objects.filter().order_by('-food_name')
     template_name = 'menu.html'
