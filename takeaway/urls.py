36bf46c0167f7467589a8643e5e76a4766878321
Colm1711 <cosullivan1711@gmail.com>
2022-09-10 13:51:29 +0000

    remove backslash from name to make navigation links more simplisitic

--- takeaway/urls.py
+++ takeaway/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='index/'),
-    path('menu/', views.Food_item_List.as_view(), name='menu/'),
+    path('', views.Home.as_view(), name='index'),
+    path('menu/', views.Food_item_List.as_view(), name='menu'),
 ]
