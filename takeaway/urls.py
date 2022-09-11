3df30ec5612632779628df097eb93e1697e4b285
Colm1711 <cosullivan1711@gmail.com>
2022-09-11 16:43:31 +0000

    Add order page to views

--- takeaway/urls.py
+++ takeaway/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.Home.as_view(), name='index'),
     path('menu/', views.Food_item_List.as_view(), name='menu'),
+    path('order/', views.Order_form.as_view(), name='order')
 ]
