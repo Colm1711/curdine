5629c832d256d525ba074a26e0b8ca8b6f0577f4
Colm1711 <cosullivan1711@gmail.com>
2022-09-11 16:13:06 +0000

    create Order model to hold data for customer order

--- takeaway/models.py
+++ takeaway/models.py
@@ -18,3 +18,24 @@ class Food_item(models.Model):
     
     def __str__(self):
         return self.food_name
+
+class Order(models.Model):
+    creation_date = models.DateTimeField(auto_now_add=True)
+    items = models.ManyToManyField(
+        'Food_item', related_name='order', blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    # user = models.ManyToManyField('User', related_name='order', blank=True)
+
+    class Meta:
+        ordering = ['-creation_date']
+
+    def __str__(self):
+        return f'Order: {self.creation_date.strftime("%m%d%Y, %H:%M:%S")}'
+
+
+# class User:
+#     pass
+
+
+# class Profile:
+#     pass
