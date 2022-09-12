41fce04e96bc63de724c016d91e62bcd51209bda
Colm1711 <cosullivan1711@gmail.com>
2022-09-12 20:53:03 +0000

    Add Profile model to extend user to hold user details

--- customer/models.py
+++ customer/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    '''
+    This is the Profile model that extends the User Profile.
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=10, blank=True)
+    address1 = models.CharField(max_length=50, blank=True)
+    address2 = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    county = models.CharField(max_length=15, blank=True)
+    eir_code = models.CharField(max_length=7, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
