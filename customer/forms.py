b4999abf9d51f076f07eb94276e709560606460c
Colm1711 <cosullivan1711@gmail.com>
2022-09-15 16:34:40 +0000

    Add view to present user with form collect the data and redirects

---
+++ customer/forms.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
+class CustForm(UserCreationForm, forms.Form):
+    email = forms.EmailField()
+    name = forms.CharField(max_length=50)
+    phone_number = forms.CharField(max_length=10)
+    address1 = forms.CharField(max_length=50)
+    address2 = forms.CharField(max_length=50)
+    city = forms.CharField(max_length=50)
+    county = forms.CharField(max_length=15)
+    eir_code = forms.CharField(max_length=7)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2',)
+        labels = {'email': 'email',
+                  'name': 'name',
+                  'phone_number': 'phone_number',
+                  'address1': 'address1',
+                  'address2': 'address2',
+                  'city': 'city',
+                  'county': 'county',
+                  'eir_code': 'eir_code', }
