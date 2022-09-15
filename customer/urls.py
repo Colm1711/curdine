34601a020f13a64bbe805b3326e59023480a4b9f
Colm1711 <cosullivan1711@gmail.com>
2022-09-15 19:55:20 +0000

    add logout url redirect

--- customer/urls.py
+++ customer/urls.py
@@ -1,6 +1,9 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('signupform/', views.cust_form, name='signupform')
+    path('signupform/', views.cust_form, name='signupform'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', views.logout, name='logout'),
 ]
