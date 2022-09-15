27a7673bbb4159f584eba4a7bb1ed4ce7f43be94
Colm1711 <cosullivan1711@gmail.com>
2022-09-15 16:08:07 +0000

    Add crispy forms for bootstrap for fomr styling

--- curdine/settings.py
+++ curdine/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'cloudinary',
     'django_summernote',
     'takeaway',
@@ -134,3 +136,8 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy form imports for bootstrap
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
