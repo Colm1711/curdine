e5179ab263041ab32f3faf79838cc46b5000ceac
Colm1711 <cosullivan1711@gmail.com>
2022-09-16 14:58:05 +0000

    Add user details for delivery to order model

--- takeaway/models.py
+++ takeaway/models.py
@@ -20,15 +20,26 @@ class Food_item(models.Model):
         return self.food_name
 
 class Order(models.Model):
+    '''
+    This is the order model for the user to
+    be able to submit order from the website with items and address.
+
+    '''
     creation_date = models.DateTimeField(auto_now_add=True)
     items = models.ManyToManyField(
         'Food_item', related_name='order', blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    # user = models.ManyToManyField('User', related_name='order', blank=True)
+    email = models.EmailField(null=False)
+    name = models.CharField(max_length=50, blank=True)
+    phone_number = models.CharField(max_length=10, blank=True)
+    address1 = models.CharField(max_length=50, blank=True)
+    address2 = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    county = models.CharField(max_length=15, blank=True)
+    eir_code = models.CharField(max_length=7, blank=True)
 
     class Meta:
         ordering = ['-creation_date']
 
     def __str__(self):
         return f'Order: {self.creation_date.strftime("%m%d%Y, %H:%M:%S")}'
-
