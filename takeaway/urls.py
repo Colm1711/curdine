eb1556ad6a5ed205474cff01e35224fd9cd60e9e
Colm1711 <cosullivan1711@gmail.com>
2022-09-16 09:23:33 +0000

    Add about me page to url pathing for site navigation

--- takeaway/urls.py
+++ takeaway/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.Home.as_view(), name='index'),
     path('menu/', views.Food_item_List.as_view(), name='menu'),
-    path('order/', views.Order_form.as_view(), name='order')
+    path('order/', views.Order_form.as_view(), name='order'),
+    path('about/', views.About.as_view(), name='about')
 ]
