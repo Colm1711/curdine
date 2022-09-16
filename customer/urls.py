15da3a2861641d326564542aa2f3fdc6cdda5af0
Colm1711 <cosullivan1711@gmail.com>
2022-09-16 19:51:54 +0000

    Add customer profile update path

--- customer/urls.py
+++ customer/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signupform/', views.cust_form, name='signupform'),
+    path('profile/update/', views.update_profile, name='update_profile'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'),
          name='login'),
     path('logout', views.logout, name='logout'),
