8a608bf101010fdb69167f63dc91e16ab5729ae5
Colm1711 <cosullivan1711@gmail.com>
2022-09-17 14:02:05 +0000

    Update the Profile admin model to have diaply list, search fields and filters

--- customer/admin.py
+++ customer/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 from .models import Profile
 
 # registering Profile Table to admin
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    '''
+    This is the class that controls the Admins view of the
+    Orders.
+
+    List how the content is presented to Amdmin User and provides list filter 
+    panel.
+
+    Provides search fields: 'name', 'address1', 'email', 'phone_number', 'city'
+    '''
+    list_display = ('name', 'address1', 'email', 'phone_number', 'city',)
+    search_fields = ['name', 'address1', 'email', 'phone_number', 'city']
+    list_filter = ('name', 'address1', 'email', 'phone_number', 'city',)
