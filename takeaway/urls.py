79f23518ae7c757129b5931925e94334b3267800
Colm1711 <cosullivan1711@gmail.com>
2022-09-17 19:15:39 +0000

    add path to food item

--- takeaway/urls.py
+++ takeaway/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.Home.as_view(), name='index'),
     path('menu/', views.Food_item_List.as_view(), name='menu'),
     path('order/', views.Order_form.as_view(), name='order'),
-    path('about/', views.About.as_view(), name='about')
+    path('about/', views.About.as_view(), name='about'),
+    path('<slug:slug>/', views.Food_Item_View.as_view(), name='food_item')
 ]
