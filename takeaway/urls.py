a0a235d16f3e166aaeff0244c91555708ebe1443
Colm1711 <cosullivan1711@gmail.com>
2022-09-20 11:56:04 +0000

    Add delete view url for user to delete reviews

--- takeaway/urls.py
+++ takeaway/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('menu/', views.Food_item_List.as_view(), name='menu'),
     path('order/', views.Order_form.as_view(), name='order'),
     path('about/', views.About.as_view(), name='about'),
-    path('<slug:slug>/', views.Food_Item_View.as_view(), name='food_item')
+    path('<slug:slug>/', views.Food_Item_View.as_view(), name='food_item'),
+    path('delete/<int:pk>/', views.ReviewDeleteView.as_view(), name='review_confirm_delete'),
 ]
