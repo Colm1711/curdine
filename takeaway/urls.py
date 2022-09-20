01cb9b3ee8dbf03bdc8b512ce86952a5425c0bf7
Colm1711 <cosullivan1711@gmail.com>
2022-09-20 11:56:39 +0000

    fixed over indented line

--- takeaway/urls.py
+++ takeaway/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('order/', views.Order_form.as_view(), name='order'),
     path('about/', views.About.as_view(), name='about'),
     path('<slug:slug>/', views.Food_Item_View.as_view(), name='food_item'),
-    path('delete/<int:pk>/', views.ReviewDeleteView.as_view(), name='review_confirm_delete'),
+    path('delete/<int:pk>/', views.ReviewDeleteView.as_view(),
+         name='review_confirm_delete'),
 ]
